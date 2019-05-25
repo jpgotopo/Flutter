3e6dd990d36b4ab5111493eb4231b7434a1ee958
Pierre Guillen <pierrealexis61196@gmail.com>
2019-05-25 12:39:04 -0500

    67. Feature Builder
    Clase terminada.

--- Flutter/componentes/lib/src/pages/home_page.dart
+++ Flutter/componentes/lib/src/pages/home_page.dart
@@ -21,33 +21,53 @@ class HomePage extends StatelessWidget {
 
     Widget _lista() {
 
-
       // print( menuProvider.opciones );
-      menuProvider.cargarData().then( (opciones){
+      // menuProvider.cargarData()
+      return FutureBuilder(
+        future: menuProvider.cargarData(),
+        // initialData: [],
+        builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot){
 
-        print( '_lista' );
-        print( opciones );
+          // print( 'builder');
+          
 
-      });
 
-      return ListView(
-        children: _listaItems(),
+          return ListView(
+          children: _listaItems( snapshot.data ),
+          );
+
+        },
       );
 
   }
 
-    List<Widget>  _listaItems() {
+    List<Widget>  _listaItems( List<dynamic> data) {
 
-      return [
 
-        ListTile( onTap: (){}, title: Text('Hello World', style: _estiloListTile,) ),
-        Divider(color: Colors.indigoAccent, ),
-        ListTile( onTap: (){}, title: Text('Hello World', style: _estiloListTile,) ),
-        Divider(color: Colors.indigoAccent, ),
-        ListTile( onTap: (){}, title: Text('Hello World', style: _estiloListTile,) ),
-        Divider(color: Colors.indigoAccent, ),
+      final List<Widget> opciones = [];
 
-      ];
+      data.forEach((opt){
+
+      
+        final widgetTemp = ListTile(
+         
+         title: Text(opt['texto'], style: _estiloListTile,), 
+         leading: Icon( Icons.adb, color: Colors.indigoAccent,),
+         trailing: Icon( Icons.keyboard_arrow_right, color: Colors.indigoAccent,),
+         onTap: (){
+
+         }, 
+
+         );
+     
+      opciones..add( widgetTemp)
+              ..add( Divider() );
+
+
+      });
+
+      return opciones;
+
 
       }
 
