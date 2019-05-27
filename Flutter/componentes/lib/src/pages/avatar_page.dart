39cfe7c0eca731b855dcb2603261c4ee9743b02a
Pierre Guillen <pierrealexis61196@gmail.com>
2019-05-26 20:40:35 -0500

    77. Circle Avatar
    <seagrega>
    - 2 Circle Avatar's
    - un FadeInImage con sus placeholder's
    </seagrega>

--- Flutter/componentes/lib/src/pages/avatar_page.dart
+++ Flutter/componentes/lib/src/pages/avatar_page.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 class AvatarPage extends StatelessWidget {
 final _estiloAppBar = new TextStyle (fontSize: 25, fontFamily: 'Dax-Regular', color: Colors.redAccent);
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,6 +11,33 @@ final _estiloAppBar = new TextStyle (fontSize: 25, fontFamily: 'Dax-Regular', co
         centerTitle: true,
         backgroundColor: Colors.lime,
         title: Text('Avatar Page | cerbero.dev ' , style: _estiloAppBar,),
+        actions: <Widget>[
+
+          Container(
+            padding: EdgeInsets.all(1.0),
+            child: CircleAvatar(
+              backgroundImage: AssetImage('assets/cerberodev.png'),
+              radius: 32.0,
+            ),
+          ),
+
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('PG'),
+              backgroundColor: Colors.redAccent,
+              radius: 32.0,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: AssetImage('assets/cerberodev.png'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 100),
+          width: 300,
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.redAccent,
