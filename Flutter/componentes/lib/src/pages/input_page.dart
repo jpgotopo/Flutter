c71cb9c20f30763b772cf20c7abf25e1e8898a48
Pierre Guillen <pierrealexis61196@gmail.com>
2019-05-27 00:08:08 -0500

    Update input_page.dart

--- Flutter/componentes/lib/src/pages/input_page.dart
+++ Flutter/componentes/lib/src/pages/input_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 
 
@@ -17,16 +18,47 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         centerTitle: true,
         backgroundColor: Colors.blue,
-        title: Text('Inputs Page | cerbero.dev ' , style: _estiloAppBar,),
+        title: Text('Inputs Page | cerbero.dev ', style: _estiloAppBar,),
       ),
-
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.greenAccent,
         child: Icon( Icons.add_to_home_screen, color: Colors.blue,),
         onPressed: (){
           Navigator.pop(context);
         },
-    ),
+      ),
+      body: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+        children: <Widget>[
+          
+          _crearInput(),
+          SizedBox(height: 10),
+          _crearInput2(),
+        ],
+      ),
     );
+  }
+
+  Widget _crearInput() {
+    return TextField(
+      //autofocus: true,
+      textCapitalization: TextCapitalization.sentences,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30)
+        ),
+        counter: Text('Letras 0'),
+        hintText: 'Name',
+        labelText: 'Name',
+        helperText: 'What Is Your Name',
+        suffixIcon: Icon(Icons.accessibility_new, color: Colors.greenAccent,),
+        icon: Icon(Icons.adb, color: Colors.greenAccent,),
+
+      ),
+    );
+  }
+
+  _crearInput2() {
+    return TextField();
   }
 }
